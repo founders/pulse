4680e5fa7bdf45f1c4b49a4a58948d026a025fea
Ben Ng <me@benng.me>
2014-01-06 13:07:09 -0800

    Add parse to accomplishment model

--- frontend/models/accomplishment.js
+++ frontend/models/accomplishment.js
@@ -3,6 +3,12 @@ var Backbone = require('backbone')
 
 Accomplishment = Backbone.Model.extend({
   url: '/accomplishments'
+, parse: function (resp) {
+    if(resp && resp.updated && typeof resp.updated == 'string')
+      resp.updated = new Date(resp.updated);
+
+    return resp;
+  }
 });
 
 module.exports = Accomplishment;
