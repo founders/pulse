29f5b1059278dcea9329ab3c87975fc949e23336
Ben Ng <me@benng.me>
2014-01-06 14:45:06 -0800

    Increase logged-in duration to 1 month

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var express = require('express')
   , errorHandler = require('./middleware/error')
   , app = express();
 
-// all environments
 app.set('port', process.env.PORT || 4000);
 app.use(express.favicon());
 app.use(express.logger(process.env.NODE_ENV == 'production' ? 'default' : 'tiny'));
@@ -23,25 +22,37 @@ app.use(express.urlencoded());
 app.use(express.cookieParser());
 app.use(express.cookieSession({
   secret: config.session.secret
-, cookie: { maxAge: 60 * 60 * 1000 }
+  // Keep folk logged in for a month at a time
+, cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
 }));
 app.use(socketio.middleware);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Logs you in
 app.post('/login', users.auth);
+// Logs you out
 app.post('/logout', users.unauth);
 
+// Tells you who you are logged in as
 app.get('/whoami', users.whoami);
+// Tells you things about a person who may/may not be registered
 app.get('/whois/:netid', users.whois);
 
+// Show all registered users
 app.get('/users', users.list);
+// Register a new user
 app.post('/users', users.create);
+// Tells you things about a user
 app.get('/users/:id', users.view);
 
+// Show all accomplishments
 app.get('/accomplishments', accomplishments.list);
+// Create a new accomplishment
 app.post('/accomplishments', accomplishments.create);
+// TODO
 app.delete('/accomplishments/:id', accomplishments.remove);
 
+// TODO
 app.get('/comments', comments.list);
 app.post('/comments', comments.create);
 app.delete('/comments/:id', comments.remove);
