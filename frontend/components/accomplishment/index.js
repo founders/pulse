6ba4108ba043f34d0091b1531f63818139e25580
Rohan Kapoor <rohan@rohankapoor.com>
2014-01-08 16:31:58 -0500

    Implemented mouseover for relative vs actual time

--- frontend/components/accomplishment/index.js
+++ frontend/components/accomplishment/index.js
@@ -7,6 +7,10 @@ var Ribcage = require('ribcage-view')
 AccomplishmentView = Ribcage.extend({
   template: require('./template.hbs')
 , className: 'pulse-accomplishment'
+, events: {
+	'mouseover .header-right'	:	'loadRealTime',
+	'mouseleave .header-right'	:	'loadRelativeTime'
+}
 , afterInit: function (opts) {
     if(!opts || !opts.model)
       throw new Error('This view must be initialized with an Accomplishment model');
@@ -19,6 +23,15 @@ AccomplishmentView = Ribcage.extend({
 	modelFields.relativeDate = relativeDate(this.model.get('updated'));
 	return modelFields;
   }
+, loadRealTime: function() {
+	this.$('.relative-date').hide();
+	this.$('.real-hidden-date').show();
+  }
+, loadRelativeTime: function() {
+	this.$('.relative-date').show();
+	this.$('.real-hidden-date').hide();
+}
+
 });
 
 module.exports = AccomplishmentView;
