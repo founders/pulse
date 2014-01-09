02db72cc7c636a4b41c60d392b96c16d0f929b8e
Rohan Kapoor <rohan@rohankapoor.com>
2014-01-08 23:43:50 -0500

    Updating relative dates for comments every minute

--- frontend/components/comment/index.js
+++ frontend/components/comment/index.js
@@ -3,13 +3,15 @@
 */
 var Ribcage = require('ribcage-view')
   , CommentView
-  , relDat = require('relative-date');
+  , relDat = require('relative-date')
+  , bind = require('lodash.bind');
 
 CommentView = Ribcage.extend({
   template: require('./template.hbs')
 , tagName: 'li'
 , className: 'comment-body topcoat-list__item'
 , loadingComments: false
+, intervalHandle: null
 , events: {
     'mouseover .signature-left' :   'loadRealTimeComment',
     'mouseleave .signature-left':   'loadRelativeTimeComment'
@@ -21,13 +23,24 @@ CommentView = Ribcage.extend({
       throw new Error('This view must be initialized with an Comment model');
 
     this.comment = opts.model;
-}
+    this.updateDates = bind(this.updateDates, this);
+  }
+, beforeRender: function() {
+  if (this.intervalHandle)
+    clearInterval(this.intervalHandle);
+  }
 , context: function () {
   var comment = this.comment.toJSON();
   comment.relativeDate = relDat(this.comment.get('updated'));
   return comment;
-}
-
+  }
+, afterRender: function() {
+  setInterval(this.updateDates, 60000);
+  }
+, beforeClose: function() {
+  if (this.intervalHandle)
+    clearInterval(this.intervalHandle);
+  }
 , loadRealTimeComment: function() {
     this.$('.comment-relative-date').hide();
     this.$('.comment-real-hidden-date').show();
@@ -35,6 +48,9 @@ CommentView = Ribcage.extend({
 , loadRelativeTimeComment: function() {
     this.$('.comment-relative-date').show();
     this.$('.comment-real-hidden-date').hide();
+  }
+, updateDates: function() {
+    this.$('.js-update-relative-date').text(relDat(this.comment.get('updated')));
   }
 });
 
